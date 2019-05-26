5074d62a697b83520c3ee63694a619b38a4cb1b1
NarcissusHliangzhao <hliangzhao@163.com>
2019-05-26 15:41:56 +0800

    Refactor the codes in Linear Regression

--- src/statistical_learning/regression/linear/lin_reg.py
+++ src/statistical_learning/regression/linear/lin_reg.py
@@ -1,6 +1,7 @@
 """
-This module provides the standard and related implementation of Linear Regression, including Standard regression
-function and corresponding shrinking strategies (Ridge regression, the LASSO, Forward stagewise regression).
+This module provides the standard and related implementations of Linear Regression, including Standard regression
+function and corresponding shrinking strategies (Locally Weighted Regression, Ridge regression, the LASSO: Forward
+stagewise regression).
 
 Author:
     Hailiang Zhao
@@ -10,6 +11,12 @@ import matplotlib.pyplot as plt
 
 
 def load_dataset(filename='../../../../dataset/regression-examples/ex0.txt'):
+    """
+    Load dataset (from folder regression-examples in default).
+
+    :param filename:
+    :return:
+    """
     features_num = len(open(filename).readline().split('\t')) - 1
     data_arr, label_arr = [], []
     fr = open(filename)
@@ -25,8 +32,7 @@ def load_dataset(filename='../../../../dataset/regression-examples/ex0.txt'):
 
 def standard_regress(data_arr, label_arr):
     """
-    This function implements the standard linear regression, and returns the calculated
-    solution.
+    This function implements the standard linear regression, and returns the calculated optimal solution.
 
     :param data_arr:
     :param label_arr:
@@ -39,20 +45,20 @@ def standard_regress(data_arr, label_arr):
         print('This matrix (X^T * X) is singular, can not do inverse!')
         return
     # what we returned is the calculated best weights under Ordinary Least Squares (OLS)
-    ws = xTx.I * (X.T * y)
-    return ws
+    weights = xTx.I * (X.T * y)
+    return weights
 
 
 def std_reg_test(data_arr, label_arr):
     """
-    This function calculated the predicted labels of data_arr by calling locally weighted
-    linear regression, and plots the figure.
+    This function calculated the predicted labels of data_arr by calling locally weighted linear regression,
+    and plots the figure.
 
     :param data_arr:
     :param label_arr:
     :return:
     """
-    we = standard_regress(data_arr, label_arr)
+    ws = standard_regress(data_arr, label_arr)
     X = np.mat(data_arr)
     y = np.mat(label_arr)
 
@@ -63,7 +69,7 @@ def std_reg_test(data_arr, label_arr):
 
     X_copy = X.copy()
     X_copy.sort(0)
-    y_predicted = X_copy * we
+    y_predicted = X_copy * ws
     ax.plot(X_copy[:, 1], y_predicted)
     plt.show()
 
@@ -72,8 +78,8 @@ def std_reg_test(data_arr, label_arr):
 
 def locally_weighted_regress(test_data, data_arr, label_arr, k=1.0):
     """
-    This function implements the locally weighted linear regression, and returns the
-    predicted value (label) of test_data.
+    This function implements the locally weighted linear regression, and returns the predicted value (label)
+    of test_data.
 
     :param test_data:
     :param data_arr:
@@ -87,22 +93,22 @@ def locally_weighted_regress(test_data, data_arr, label_arr, k=1.0):
     m = np.shape(X)[0]
     # use a kernel function to weight nearby points more heavily than other points
     # the 'distance' is from test_data (an instance) to data-arr (size m)
-    weights = np.mat(np.eye(m))
+    ws_from_kernel = np.mat(np.eye(m))
     for j in range(m):
         diff_mat = test_data - X[j, :]
-        weights[j, j] = np.exp(diff_mat * diff_mat.T / (-2.*k**2))
-    xTx = X.T * (weights * X)
+        ws_from_kernel[j, j] = np.exp(diff_mat * diff_mat.T / (-2.*k**2))
+    xTx = X.T * (ws_from_kernel * X)
     if np.linalg.det(xTx) == 0.:
         print('This matrix (X^T * X) is singular, can not do inverse!')
         return
-    ws = xTx.I * (X.T * (weights * y))
-    return test_data * ws
+    weights = xTx.I * (X.T * (ws_from_kernel * y))
+    return test_data * weights
 
 
 def lw_reg_test(test_arr, data_arr, label_arr, k=1.0):
     """
-    This function calculated the predicted labels of test_arr by calling locally weighted
-    linear regression, and plots the figure.
+    This function calculated the predicted labels of test_arr by calling locally weighted linear regression,
+    and plots the figure.
 
     If the data instances have multiple dimensions (>=2), we only plot 'the value second dimension' vs.
     'the value of label'.
@@ -137,8 +143,7 @@ def lw_reg_test(test_arr, data_arr, label_arr, k=1.0):
 
 def ridge_regress(X, y, lam=0.2):
     """
-    This function implements the ridge linear regression, and returns the calculated
-    solution.
+    This function implements the ridge linear regression, and returns the calculated solution.
 
     When we have more data points than features, the matrix xTx is surely not fully-ranked,
     thus it is non-singular. As a result, xTx does not have inverse. We add a user-defined
@@ -150,7 +155,9 @@ def ridge_regress(X, y, lam=0.2):
     The equation for ridge regression is the same as our regular least-squares regression and
     imposing the following constraint: \sum_{k=1}^n w_k^2 \leq \lambda.
 
-    Thus, Ridge Regression is the implementation of regularization.
+    Thus, Ridge Regression is an implementation of regularization. This is so-called Shrinkage
+    Method, which imposes a constraint on the size of the regression coefficients. Shrinkage
+    methods can also be viewed as adding bias to a model and reducing the variance.
 
     :param X:
     :param y:
@@ -160,19 +167,20 @@ def ridge_regress(X, y, lam=0.2):
     xTx = X.T * X
     regularized_xTx = xTx + np.eye(np.shape(X)[1]) * lam
     if np.linalg.det(regularized_xTx) == 0.:
-        print('The normalized matrix is singular, can not do inverse!')
+        print('The regularized matrix is singular, can not do inverse!')
         return
-    ws = regularized_xTx.I * (X.T * y)
-    return ws
+    weights = regularized_xTx.I * (X.T * y)
+    return weights
 
 
-def compare_ridges(data_arr, label_arr):
+def compare_ridges(data_arr, label_arr, lambda_num=30):
     """
     This function compares 30 different lambdas in Ridge Regression  adn returns the calculated
     solutions in ws_mat.
 
     :param data_arr:
     :param label_arr:
+    :param lambda_num:
     :return:
     """
     X, y = np.mat(data_arr), np.mat(label_arr).T
@@ -182,11 +190,9 @@ def compare_ridges(data_arr, label_arr):
     # we normalized our data to give each feature equal importance regardless of the units it was measured in
     y_mean = np.mean(y, 0)
     y = y - y_mean
-    X_means = np.mean(X, 0)
-    X_var = np.var(X, 0)
+    X_means, X_var = np.mean(X, 0), np.var(X, 0)
     X = (X - X_means) / X_var
 
-    lambda_num = 30
     ws_mat = np.zeros((lambda_num, np.shape(X)[1]))
     for i in range(lambda_num):
         ws = ridge_regress(X, y, np.exp(i-10))
@@ -204,8 +210,8 @@ def compare_ridges(data_arr, label_arr):
 
 def ridge_reg_test(data_arr, label_arr, lam=0.2):
     """
-        This function calculated the predicted labels of data_arr by calling ridge
-        linear regression, and plots the figure.
+        This function calculated the predicted labels of data_arr by calling ridge linear regression,
+        and plots the figure.
 
         :param data_arr:
         :param label_arr:
@@ -213,13 +219,12 @@ def ridge_reg_test(data_arr, label_arr, lam=0.2):
         :return:
         """
     X, y = np.mat(data_arr), np.mat(label_arr).T
-    we = ridge_regress(X, y, lam)
-    y_predicted = X * we
-
+    ws = ridge_regress(X, y, lam)
+    y_predicted = X * ws
     return y_predicted
 
 
-def forward_stagewise_regress(data_arr, label_arr, eps=0.01, iterations=100):
+def forward_stagewise_regress(data_arr, label_arr, eps=0.001, iterations=1000):
     """
     This function implements the Forward Stagewise Regression, and returns the calculated solutions in
     different iterations.
@@ -230,28 +235,70 @@ def forward_stagewise_regress(data_arr, label_arr, eps=0.01, iterations=100):
     To solve this we now need a quadratic programming algorithm. Instead of using the quadratic solver,
     Forward Stagewise Regression is adopted, which is an easier algorithm to implements LASSO.
 
+    Thus, Forward Stagewise Regression is an implementation of regularization. This is so-called Shrinkage
+    Method, which imposes a constraint on the size of the regression coefficients. Shrinkage methods can
+    also be viewed as adding bias to a model and reducing the variance.
+
     :param data_arr:
     :param label_arr:
     :param eps: the step size to take at each iteration
     :param iterations: the number of iterations
     :return:
     """
-    pass
+    X, y = np.mat(data_arr), np.mat(label_arr).T
+    # normalization
+    y_mean = np.mean(y, 0)
+    y = y - y_mean
+    X_means, X_var = np.mean(X, 0), np.var(X, 0)
+    X = (X - X_means) / X_var
+
+    m, n = np.shape(X)
+    ws = np.zeros((n, 1))
+    ws_max = ws.copy()
+    ws_mat = np.zeros((iterations, n))
+
+    for i in range(iterations):
+        # print(ws.T)
+        min_error = np.inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                ws_test = ws.copy()
+                ws_test[j] += eps * sign
+                y_test = X * ws_test
+                error = residual_sum_squares(y.A, y_test.A)
+                if error < min_error:
+                    min_error = error
+                    ws_max = ws_test
+        ws = ws_max.copy()
+        ws_mat[i, :] = ws.T
+
+    # plot figure on coefficients vs. iterations
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    ax.plot(ws_mat)
+    plt.show()
+
+    return ws_mat
 
 
-def forward_stagewise_reg_test(data_arr, label_arr, eps=0.01, iterations=100):
-    pass
+def forward_stagewise_reg_test(data_arr, label_arr, eps=0.001, iterations=1000):
+    X, y = np.mat(data_arr), np.mat(label_arr).T
+    ws = forward_stagewise_regress(data_arr, label_arr, eps, iterations)[iterations-1]
+    y_predicted = X * np.mat(ws).T
+    return y_predicted
 
 
 def abalone_test():
+    # we only use the first 100 instances as training set, the following 100 instances as the test set
     data_arr, label_arr = load_dataset('../../../../dataset/regression-examples/abalone.txt')
     print('===> Test Standard Regression <===')
-    we = standard_regress(data_arr[0:99], label_arr[0:99])
-    y_predicted = np.mat(data_arr[0:99]) * we
+    ws = standard_regress(data_arr[0:99], label_arr[0:99])
+    y_predicted = np.mat(data_arr[0:99]) * ws
     print('Residual Sum of Squares (RSS) error on training set is: %f' %
           (residual_sum_squares(label_arr[0:99], y_predicted.T.A)))
-    y_predicted = np.mat(data_arr[100:199]) * we
-    print('Residual Sum of Squares (RSS) error on test set is: %f' %
+    y_predicted = np.mat(data_arr[100:199]) * ws
+    print('Residual Sum of Squares (RSS) error on test set is: %f\n' %
           (residual_sum_squares(label_arr[100:199], y_predicted.T.A)))
 
     print('===> Test Locally Weighted Linear Regression <===')
@@ -268,16 +315,21 @@ def abalone_test():
     print('===> Test Ridge Linear Regression <===')
     for lam in [np.exp(-5), 0.02, np.exp(5)]:
         print('lambda = %f' % lam)
-        we = ridge_regress(np.mat(data_arr[0:99]), np.mat(label_arr[0:99]).T, lam)
-        y_predicted = np.mat(data_arr[0:99]) * we
+        ws = ridge_regress(np.mat(data_arr[0:99]), np.mat(label_arr[0:99]).T, lam)
+        y_predicted = np.mat(data_arr[0:99]) * ws
         print('Residual Sum of Squares (RSS) error on training set is: %f' %
               (residual_sum_squares(label_arr[0:99], y_predicted.T.A)))
-        y_predicted = np.mat(data_arr[100:199]) * we
-        print('Residual Sum of Squares (RSS) error on test set is: %f' %
+        y_predicted = np.mat(data_arr[100:199]) * ws
+        print('Residual Sum of Squares (RSS) error on test set is: %f\n' %
               (residual_sum_squares(label_arr[100:199], y_predicted.T.A)))
 
     print('===> Test Forward Stagewise Regression <===')
-    pass
+    for it in [1000, 2000, 10000]:
+        print('iterations: %d' % it)
+        ws = forward_stagewise_regress(data_arr[0:99], label_arr[0:99], iterations=it)[it-1]
+        y_predicted = np.mat(data_arr[0:99]) * np.mat(ws).T
+        print('Residual Sum of Squares (RSS) error on training set is: %f\n' %
+              (residual_sum_squares(label_arr[0:99], y_predicted.T.A)))
 
 
 def get_correlation(y_predicted, y_actual):
@@ -305,5 +357,6 @@ if __name__ == '__main__':
     # data, labels = load_dataset('../../../../dataset/regression-examples/abalone.txt')
     # compare_ridges(data, labels)
     # ridge_reg_test(data, labels, lam=0.02)
+    # forward_stagewise_reg_test(data, labels, eps=0.01, iterations=500)
 
     abalone_test()
